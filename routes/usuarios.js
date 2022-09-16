5c216e624b817899f3d4af025d6e106adfc25a95
Gabriel <gabrielcalmeida1804@gmail.com>
2022-09-16 15:03:17 -0300

    jwt and controllers

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,100 +1,11 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-router.post('/cadastro', (req, res, next) => {
-    mysql.getConnection((err, conn) => {
-        if (err) { return res.status(500).send({ error: error }) };
-        conn.query('SELECT * FROM usuarios WHERE email= ?', [req.body.email], (error, results) => {
-            if (error) { return res.status(500).send({ error: error }) };
-            if (results.length > 0) {
-                res.status(409).send({ mensagem: 'Usuário existente' })
-            } else {
-                bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-                    if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) };
-                    conn.query(
-                        `INSERT INTO usuarios (email, password) values (?,?)`,
-                        [req.body.email, hash],
-                        (error, results) => {
-                            conn.release();
-                            if (error) { return res.status(500).send({ error: error }) };
-                            response = {
-                                mensagem: 'Usuário criado com sucesso',
-                                usuarioCriado: {
-                                    user_id: results.insertId,
-                                    email: req.body.email
-                                }
-                            }
-                            return res.status(201).send(response);
-                        }
-                    );
-                });
-            };
-        });
-    });
-});
-router.post('/login', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) };
-        const query = `SELECT * FROM usuarios WHERE email =?`;
-        conn.query(query, [req.body.email], (error, results, fields) => {
-            conn.release();
-            if (error) { return res.status(500).send({ error: error }) };
-            if (results.length < 1) {
-                return res.status(401).send({ mensagem: 'Unauthorized' });
-            };
-            bcrypt.compare(req.body.password, results[0].password, (err, result) => {
-                if (err) {
-                    return res.status(401).send({ mensagem: 'Unauthorized' });
-                }
-                if (result) {
-                    let token = jwt.sign({
-                        id_user: results[0].id_user,
-                        email: results[0].email
-                    },
-                        `${process.env.JWT_KEY}`,
-                        {
-                            expiresIn: "1h"
-                        });
-                    return res.status(200).send({
-                        mensagem: 'Authorized',
-                        token: token
-                    });
-                }
-                return res.status(401).send({ mensagem: 'Unauthorized' });
-            });
-        });
-    });
-});
-router.get('/', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM usuarios',
-            (error, result, fields) => {
-                conn.release();
-                if (error) { return res.status(500).send({ error: error }) };
-                const response = {
-                    usuarios: result.map(usuario => {
-                        return {
-                            user_id: usuario.user_id,
-                            email: usuario.email,
-                            request: {
-                                tipo: 'POST',
-                                descricao: 'Adiciona um novo usario',
-                                url: 'http://localhost:3000/usuarios/cadastro'
-                            }
-                        }
-                    })
+const usuariosController = require('../controllers/usuarios-controller');
 
-                };
-                res.status(201).send(response);
-            }
-        )
-    });
-});
+router.post('/cadastro', usuariosController.PostCadastro);
+router.post('/login', usuariosController.PostLogin);
+router.get('/', usuariosController.GetUsuarios);
 
 module.exports = router;
