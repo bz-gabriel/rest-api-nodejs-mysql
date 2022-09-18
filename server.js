10a254e503e7c77a35273912a31ade9097008320
Gabriel <gabrielcalmeida1804@gmail.com>
2022-09-18 11:47:39 -0300

    incluido procfie

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const http = require('http');
 const app = require('./app');
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 
