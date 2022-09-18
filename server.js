1448586a19f20c4652d1fe24e404ed46c0298bc8
Gabriel <gabrielcalmeida1804@gmail.com>
2022-09-18 11:04:33 -0300

    alteração da porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(port);
 
